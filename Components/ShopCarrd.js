7d3d0712bffbcaf1de173d47a913e9b27e0a5632
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-05-31 04:15:49 +0100

    init

---
+++ Components/ShopCarrd.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ShopCarrd = () => {
+  return (
+    <View>
+      <Text>ShopCarrd</Text>
+    </View>
+  )
+}
+
+export default ShopCarrd
