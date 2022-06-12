d1b2b0994bb27340e37d114987074b5e3a1ceab3
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-12 08:05:28 +0100

    init

---
+++ screens/SellerAddProduct.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-nativion e'
+import React from 'react'
+
+const SellerAddProduct = ({navigat}) => {
+  return (
+    <View>
+      <Text>SellerAddProduct</Text>
+    </View>
+  )
+}
+
+export default SellerAddProduct
